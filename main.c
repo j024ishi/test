c66292435fd951ed29b6a0c2ce5720865f328463
j024ishi <j520024k@mails.cc.ehime-u.ac.jp>
2024-06-25 19:22:16 +0900

    int yをdouble yに変更した

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないときエラー表示を行い exit(1) とする。 
 {
-  int y;
+  double y;
   if( x <= 0 ){
     printf("エラー：0以上の値ではありません in myRoot()\n");　      
     exit(1);
